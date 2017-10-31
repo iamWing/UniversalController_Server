e04a45011fb8543781d51de74b9c877b88880df9
Wing Chau <wing@devtography.com>
2017-10-31 05:45:04 +0000

    Implemented request method for socket permission
    Implementing the socket server using native .NET socket APIs in TCP mode instead of UDP as stream isn't available in UDP mode.

--- Assets/UniversalController/Server.cs
+++ Assets/UniversalController/Server.cs
@@ -1,39 +1,56 @@
-﻿using System.IO;
-using UnityEngine.Networking;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace AlphaOwl.UniversalController
 {
-	/// <summary>
-	/// Main class of the library & initialises a signle 
-	/// socket server as the host within the game.
-	/// </summary>
-	public class Server
-	{
-		private const int DefaultPort = 28910;
-		private const int DefaultMaxConnections = 4;
+    /// <summary>
+    /// Main class of the library & initialises a signle 
+    /// socket server as the host within the game.
+    /// </summary>
+    public class Server
+    {
+        private const int DefaultPort = 28910;
+        private const int DefaultMaxConnections = 4;
 
-		private static bool IsRunning = false;
+        private static bool IsRunning = false;
 
-		private static int Port;
-		private static int MaxConnections;
+        private static int Port;
+        private static int MaxConnections;
 
-		/// <summary>
-		/// Create a new instance of Server that will be initialised 
-		/// with specified port number & maximum connections.
-		/// </summary>
-		/// <param name="port">Port number of the server.</param>
-		/// <param name="maxConnections">Maximum connections of the server.</param>
-		public static Server Init(int port = DefaultPort, int maxConnections = DefaultMaxConnections)
-		{
-			Server server = new Server();
+        private Socket socketListener;
+        private Socket handler;
+        private IPEndPoint ipEndPoint;
 
-			server.CreateHost(port, maxConnections);
-		}
+        /// <summary>
+        /// Create a new instance of Server that will be initialised 
+        /// with specified port number & maximum connections.
+        /// </summary>
+        /// <param name="port">Port number of the server.</param>
+        /// <param name="maxConnections">Maximum connections of the 
+        /// server.</param>
+        public static Server Init(int port = DefaultPort, 
+                                  int maxConnections = DefaultMaxConnections)
+        {
+            Server server = new Server();
 
-		private void CreateHost(int port, int maxConnections)
-		{
-			Port = port;
-			MaxConnections = maxConnections;
-		}
-	}
+        }
+
+        /// <summary>
+        /// Requests the socket permission.
+        /// </summary>
+        private void RequestSocketPermission()
+        {
+            // Creates SocketPermission object for access restrictions
+            SocketPermission permission = 
+                new SocketPermission(
+                    NetworkAccess.Accept, // Allowes to accept connections
+                    TransportType.Tcp, // Defines transport types
+                    "", // The IP addresses of local host
+                    SocketPermission.AllPorts // Applies to all ports
+                );
+
+            // Ensures the code to have permission to access a socket
+            permission.Demand();
+        }
+    }
 }
