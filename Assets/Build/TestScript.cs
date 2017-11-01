8aac7f370da9ae7027f59f0a69caf2489d3cae1f
Wing Chau <wing@devtography.com>
2017-11-01 06:49:04 +0000

    Added test scene & script

---
+++ Assets/Build/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AlphaOwl.UniversalController;
+
+public class TestScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Server server = Server.Init(debug: true);
+        server.Start();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
