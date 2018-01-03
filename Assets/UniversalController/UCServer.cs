0c4a577e2185b552b48fbba0e6fdeb13f387de52
Wing Chau <wing@devtography.com>
2018-01-03 09:30:22 +0000

    Created UCServer.cs
    Script that's to replace Server.cs.

---
+++ Assets/UniversalController/UCServer.cs
@@ -0,0 +1,32 @@
+﻿using AlphaOwl.UniversalController.Utilities;
+
+namespace AlphaOwl.UniversalController
+{
+    public class UCServer : NetworkUtilities.IMessageReceiver,
+    NetworkUtilities.IMessageSender
+    {
+        /* Override methods from NetworkUtilities.IMessageReceiver */
+
+        public void OnReceiveComplete(string msg)
+        {
+
+        }
+
+        public void OnReceiveFail(string err)
+        {
+
+        }
+
+        /* Override methods from NetworkUtilities.IMessageSender */
+
+        public void OnSendComplete()
+        {
+
+        }
+
+        public void OnSendFail(string err)
+        {
+
+        }
+    }
+}
