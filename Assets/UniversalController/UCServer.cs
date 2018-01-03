8299572c7fc2da13e2bc6b4356422ee0faeec804
Wing Chau <wing@devtography.com>
2018-01-03 10:00:57 +0000

    Defined default values
    Defined default port & maximum connections. And set constructor as private.

--- Assets/UniversalController/UCServer.cs
+++ Assets/UniversalController/UCServer.cs
@@ -1,10 +1,32 @@
-﻿using AlphaOwl.UniversalController.Utilities;
+﻿using System.Net.Sockets;
+using AlphaOwl.UniversalController.Utilities;
 
 namespace AlphaOwl.UniversalController
 {
     public class UCServer : NetworkUtilities.IMessageReceiver,
     NetworkUtilities.IMessageSender
     {
+        // Default variables.
+        private const int DefaultPort = 28910;
+        private const int DefaultMaxConnections = 4;
+
+        private Socket serverSocket;
+        private string[] players; // Connected players.
+
+        /// <summary>
+        /// Private constructor that prevents initialisation 
+        /// of an instance of UCServer by using the default 
+        /// constructor.
+        /// </summary>
+        private UCServer(int port, int maxConn)
+        {
+            serverSocket = NetworkUtilities.InitSocketServer(
+                            NetworkUtilities.GetIpAddress(), 
+                            port);
+
+            players = new string[maxConn];
+        }
+
         /* Override methods from NetworkUtilities.IMessageReceiver */
 
         public void OnReceiveComplete(string msg)
