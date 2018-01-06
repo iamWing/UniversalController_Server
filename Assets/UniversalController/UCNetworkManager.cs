019b748a33cdb78b96cfb414865032cc0f972774
Wing Chau <wing@devtography.com>
2018-01-06 03:34:41 +0000

    Implemented server shutdown

--- Assets/UniversalController/UCNetworkManager.cs
+++ Assets/UniversalController/UCNetworkManager.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
+using AlphaOwl.UniversalController.Utilities;
 
 namespace AlphaOwl.UniversalController
 {
     public class UCNetworkManager : MonoBehaviour, UCServer.ICommandHandler
     {
         private static UCNetworkManager instance;
+        private static UCServer server;
 
         [SerializeField] protected GameObject playerPrefab;
 
         [SerializeField] protected int portNumber;
         [SerializeField] protected int maxConnections;
         [SerializeField] protected bool debug = false;
-
-        private UCServer server;
 
         void Awake()
         {
@@ -22,7 +22,33 @@ namespace AlphaOwl.UniversalController
         // Use this for initialization
         void Start()
         {
-            server = UCServer.Init(this, portNumber, maxConnections, debug);
+            if (server == null)
+            {
+                server = UCServer.Init(this, portNumber,
+                maxConnections, debug);
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            ServerShutdown();
+        }
+
+        /// <summary>
+        /// Shutdown the sever.
+        /// </summary>
+        public static void ServerShutdown()
+        {
+            if (server != null)
+            {
+                server.Shutdown();
+                DebugUtilities.Log("Server is now shutting down...");
+            }
+            else
+            {
+                DebugUtilities.Log(msg: "Server does not exists.",
+                type: Utilities.LogType.Warning);
+            }
         }
 
         /// <summary>
