8323e5d2532ca478d825f657bd6357e48f042665
Wing Chau <wing@devtography.com>
2018-01-06 01:38:22 +0000

    Defined command lengths
    Added comments for command formats.

--- Assets/UniversalController/UCServer.cs
+++ Assets/UniversalController/UCServer.cs
@@ -13,13 +13,46 @@ namespace AlphaOwl.UniversalController
         public const char Separator = ':';
 
         /* For commands received */
+
+        /// <summary>
+        /// example - REGISTER:{player_name}
+        /// whereas player_name : string
+        /// </summary>
         public const string Register = "REGISTER";
+        public const int RegisterLength = 2;
+
+        /// <summary>
+        /// example - DEREGISTER:{player_id}
+        /// whereas player_id : int
+        /// </summary>
         public const string Deregister = "DEREGISTER";
+        public const int DeregisterLength = 2;
 
         // Player actions
+
+        /// <summary>
+        /// example - {player_id}:KEY_DOWN:{key}
+        /// example w/ extra - {player_id}:KEY_DOWN:{key}:{extra}
+        /// whereas player_id : int; key : string; extra : string
+        /// </summary>
         public const string KeyDown = "KEY_DOWN";
+        public const int KeyDownLength = 3;
+        public const int KeyDownExtraLength = 4;
+
+        
+        /// <summary>
+        /// example - {player_id}:JOYSTICK:{x}:{y}
+        /// whereas player_id : int; x : float; y : float;
+        /// </summary>
         public const string Joystick = "JOYSTICK";
+        public const int JoystickLength = 4;
+
+        /// <summary>
+        /// example - {player_id}:GYRO:{x}:{y}:{z}
+        /// whereas player_id : int; x : float; y : float; z : float;
+        /// </summary>
         public const string Gyro = "GYRO";
+        public const int GyroLength = 5;
 
         // Replies to client
         public const string PlayerId = "PLAYER_ID";
