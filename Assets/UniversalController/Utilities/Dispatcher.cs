45b66e8488aa2df860a255a704728f87a7e8c2ec
Wing Chau <wing@devtography.com>
2018-01-07 04:39:07 +0000

    Created Dispatcher.cs
    Added a dispatcher interface & class to schedule code for execution in
    main thread.

---
+++ Assets/UniversalController/Utilities/Dispatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphaOwl.UniversalController.Utilities
+{
+    public interface IDispatcher
+    {
+        void Invoke(Action fn);
+    }
+
+    public class Dispatcher
+    {
+        public List<Action> pending = new List<Action>();
+
+        /// <summary>
+        /// Schedule code for execution in the thread.
+        /// </summary>
+        /// <param name="fn">Function that needs to be 
+        /// invoked.</param>
+        public void Invoke(Action fn)
+        {
+            lock (pending)
+            {
+                pending.Add(fn);
+            }
+        }
+
+        /// <summary>
+        /// Execute pending actions.
+        /// </summary>
+        public void InvokePending()
+        {
+            lock(pending)
+            {
+                foreach (var action in pending)
+                    action();
+
+                pending.Clear();
+            }
+        }
+    }
+}
